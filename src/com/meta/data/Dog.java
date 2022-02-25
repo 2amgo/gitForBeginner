73cb77fc82aa0afc514778a2f4703710075b93a3
meta <96930703+2amgo@users.noreply.github.com>
2022-02-25 10:03:27 +0700

    Update Dog.java

--- src/com/meta/data/Dog.java
+++ src/com/meta/data/Dog.java
@@ -5,5 +5,7 @@ public class Dog extends  Pet{
     public Dog(String id, String name, int yob, double weight) {
         super(id, name, yob, weight);
     }
-
+    public void run(){
+      System.out.println("this is ahihi ");
+    }
 }
