6fed8f36aaaaf08a7479c1bce2b199368b0f73aa
2AMgo <phucthinh.sun@gmail.com>
2022-02-25 09:54:44 +0700

    add first time

---
+++ src/com/meta/stage/Stage.java
@@ -0,0 +1,10 @@
+package com.meta.stage;
+
+import com.meta.data.Pet;
+
+public class Stage {
+    public static void main(String[] args) {
+        Pet hoaMi   = new Pet("b01", "hoa mi hot trong dem ", 2, 1.2);
+        hoaMi.showInfor();
+    }
+}
